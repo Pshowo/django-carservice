048c06227d006f06a340afe099aff2b4c82910c6
Pawel <pjuszcze@gmail.com>
2020-12-27 00:03:06 +0100

    Added: First view on tickets

--- tickets/views.py
+++ tickets/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    http="<h2>Welcome to the Hypercar Service!</h2>"
+    return HttpResponse(http)
