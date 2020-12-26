d9e833611eef9b659cf6d8a17a564d0c8c9e38e9
Pawel <pjuszcze@gmail.com>
2020-12-27 00:08:07 +0100

    Changed: view using by class View

--- tickets/views.py
+++ tickets/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
 
-# Create your views here.
-def index(request):
-    http="<h2>Welcome to the Hypercar Service!</h2>"
-    return HttpResponse(http)
+class WelcomeView(View):
+    def get(self, request, *args, **kwargs):
+        http="<h2>Welcome to the Hypercar Service!</h2>"
+        return HttpResponse(http)        
